4642677ebf2ef99aed8bc508e49619cec2c45c49
webelf101 <m63974529@gmail.com>
2023-08-24 00:56:30 +0800

    bugfix

--- limit-orders.js
+++ limit-orders.js
@@ -281,7 +281,7 @@ async function getNewBlocks(fromBlock) {
             const stop_loss = deposited_event.returnValues["stop_loss"];
             const insert_profit_taking = Number(profit_taking) + Number(SLIPPAGE);
             const insert_stop_loss = Number(stop_loss) > Number(SLIPPAGE) ? Number(stop_loss) - Number(SLIPPAGE) : 0;
-            flat_array.push(deposited_event.returnValues["deposit_id"]);
+            flat_array.push(Number(deposited_event.returnValues["deposit_id"]));
             flat_array.push(deposited_event.returnValues["token0"]);
             flat_array.push(deposited_event.returnValues["token1"]);
             flat_array.push(deposited_event.returnValues["amount0"]);
